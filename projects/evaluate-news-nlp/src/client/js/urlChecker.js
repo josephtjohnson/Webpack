1afadd11c66f13eaa95cc4639468de1029e06fcc
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2022-05-18 13:48:08 -0400

    Update urlChecker.js

--- projects/evaluate-news-nlp/src/client/js/urlChecker.js
+++ projects/evaluate-news-nlp/src/client/js/urlChecker.js
@@ -1,8 +1,10 @@
 function checkForURL(inputURL) {
     console.log("::: Running checkForURL :::", inputURL);
-    
-    inputURL.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
     if (inputURL == null) {
+        alert.('URL cannot be blank');
+        return false;
+    }
+    else if (!inputURL.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)) {
         alert.('Please use a valid URL');
         return false;
     }
