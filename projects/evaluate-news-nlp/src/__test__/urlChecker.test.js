2b60eb573c6b9e2017d9af88f9b7a7716450d136
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2022-05-18 13:45:43 -0400

    Update urlChecker.test.js

--- projects/evaluate-news-nlp/src/__test__/urlChecker.test.js
+++ projects/evaluate-news-nlp/src/__test__/urlChecker.test.js
@@ -3,4 +3,14 @@ import {checkForName } from "../src/client/js/urlChecker"
 describe ("Testing the url check functionality", () => {
   test("Testing the checkForURL() function", () => {
     expect(checkForURL).toBeDefined();
-})});
+  });
+  test("Testing the url is not null", () => {
+    const url = 'https://apnews.com/article/elon-musk-technology-a71c76ed753b144f4aee081df5ab707c';
+    expect(urlChecker(url).toBeTruthy();
+  });
+    test("Testing the url is null", () => {
+    const url = '';
+    expect(urlChecker(url).toBeNull();
+  });
+  
+});
