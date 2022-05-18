d338c9b15f6793ced782d9250fe8a95a447d5846
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2022-05-18 13:30:00 -0400

    Update and rename nameChecker.js to urlChecker.js

---
+++ projects/evaluate-news-nlp/src/client/js/urlChecker.js
@@ -0,0 +1,8 @@
+function checkForURL(inputURL) {
+    console.log("::: Running checkForURL :::", inputURL);
+    
+    inputURL.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    return (inputURL !== null)
+};
+
+export { checkForName }
