ddc1926cf33b686058e597099630b94052ee1d19
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2022-05-18 12:04:16 -0400

    Update apiCall to handle empty url

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,14 +1,15 @@
+//setup for api
 const dotenv = require('dotenv');
 dotenv.config();
 const apiKey = process.env.API_KEY;
 const endpoint = process.env.END_POINT;
 console.log(`Your API key is ${process.env.API_KEY}`);
 
-var path = require('path')
+const path = require('path')
 const express = require('express')
 
 //mockAPI for testing
-const mockAPIResponse = require('./mockAPI.js')
+//const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
 
@@ -25,28 +26,27 @@ app.use(cors());
 
 console.log(__dirname)
 
+//GET response for homepage
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
-// designates what port the app will listen to for incoming requests
+//designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8080!')
+    console.log('Example app listening on port 8081!')
 })
 
+/*mock api for testing
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+*/
 
 const apiCall = async(req, res) => {
-    if (Object.keys(req.body).length === 0) {
-        res.status(400).send('Please ensure your input is not empty');
-        res.send('API not accessed');
-    }
-    const txt = req.body;
-    console.log(dataCall);
-    const response = await fetch ('${endpoint}key=${apiKey}txt=${txt}&lang=en');
+    if (Object.keys(req.body).length === 0) res.status(404).send('Please use a valid URL');
+    console.log(req.body);
+    const response = await fetch ('${endpoint}key=${apiKey}url=${req.body.text}&lang=en');
     try {
         const data = await response.json();
         console.log(data);
