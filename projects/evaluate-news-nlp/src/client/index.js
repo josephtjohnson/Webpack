ce86b0d6c6d0068a1ef8ba25c7532a3a09745e4c
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2022-05-18 13:30:40 -0400

    Update index.js

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -3,15 +3,15 @@ import './styles/base.scss';
 import './styles/footer.scss';
 import './styles/form.scss';
 import './styles/header.scss';
-import { checkForName } from './js/nameChecker';
+import { checkForURL } from './js/urlChecker';
 import { handleSubmit } from './js/formHandler';
 
-console.log(checkForName);
+console.log(checkForURL);
 
 alert("I EXIST")
 console.log("CHANGE!!");
 
 export {
- checkForName,
+ checkForURL,
  handleSubmit
 }
