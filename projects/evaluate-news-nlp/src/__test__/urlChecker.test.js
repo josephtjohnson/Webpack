9633be053d06ab0dc68fbd1b78b880380da4bae0
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2022-05-18 13:50:02 -0400

    Add check for bad url

--- projects/evaluate-news-nlp/src/__test__/urlChecker.test.js
+++ projects/evaluate-news-nlp/src/__test__/urlChecker.test.js
@@ -6,11 +6,14 @@ describe ("Testing the url check functionality", () => {
   });
   test("Testing the url is not null", () => {
     const url = 'https://apnews.com/article/elon-musk-technology-a71c76ed753b144f4aee081df5ab707c';
-    expect(urlChecker(url).toBeTruthy();
+    expect(urlChecker(url)).toBeTruthy();
   });
-    test("Testing the url is null", () => {
+  test("Testing the url is null", () => {
     const url = '';
-    expect(urlChecker(url).toBeNull();
+    expect(urlChecker(url)).toBeNull();
   });
-  
+  test("Testing the url is null", () => {
+    const url = 'adsfafdhadsf';
+    expect(urlChecker(url)).toBeFalsy();
+  });  
 });
